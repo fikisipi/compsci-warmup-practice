92e515d4b004b5329e46230b8403fdc44f3f26a9
Filip Dimitrovski <filip.dimitrovski@inscale.net>
2025-05-12 11:16:36 +0000

    warmup setup

---
+++ modal-machinelearning/llamacpp.py
@@ -0,0 +1,21 @@
+import modal
+
+cuda_version = "12.4.0"
+flavor = "devel"
+operating_sys = "ubuntu22.04"
+tag = f"{cuda_version}-{flavor}-{operating_sys}"
+
+image = (
+    modal.Image.from_registry(f"nvidia/cuda:{tag}", add_python="3.12")
+    .apt_install("git", "build-essential", "cmake", "curl", "libcurl4-openssl-dev")
+    .run_commands("git clone https://github.com/ggerganov/llama.cpp")
+    .run_commands(
+        "cmake llama.cpp -B llama.cpp/build "
+        "-DBUILD_SHARED_LIBS=OFF -DGGML_CUDA=ON -DLLAMA_CURL=ON "
+    )
+    .run_commands(
+        "cmake --build llama.cpp/build --config Release -j --clean-first --target llama-quantize llama-cli"
+    )
+    .run_commands("cp llama.cpp/build/bin/llama-* llama.cpp")
+    .entrypoint([])
+)
